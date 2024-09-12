965e39fb9bb4383f380f3c2d0e320ab67af45bd6
dstrzykalski <dstrzykalski@yahoo.com>
2024-09-12 18:59:06 -0400

    Database File Commit

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,35 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // Open the database
+  const db = await openDB('jate', 1);
+  // Uses the Transaction Method
+  const transaction = db.transaction('jate', 'readwrite');
+  // Uses the Object Store Method
+  const store = transaction.objectStore('jate');
+  // Uses the Add Method
+  await store.add({ content });
+  // Uses the Transaction Done Method
+  await transaction.done;
+  console.log('Success!');
+};
+
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  // Open the database
+  const db = await openDB('jate', 1);
+  // Uses the Transaction Method
+  const transaction = db.transaction('jate', 'readonly');
+  // Uses the Object Store Method
+  const store = transaction.objectStore('jate');
+  // Uses the Get All Method
+  const content = await store.getAll();
+  // Uses the Transaction Done Method
+  await transaction.done;
+  return content;
+};
 
 initdb();
